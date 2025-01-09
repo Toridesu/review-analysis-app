c17adcf32568684bcc3193a9675207ec53b84c7d
Toridesu <160250540+Toridesu@users.noreply.github.com>
2025-01-09 12:36:08 +0900

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? 'REPOSITORY_NAME' : './',
+  base: process.env.GITHUB_PAGES ? '/review-app/' : './',
   plugins: [react()],
   resolve: {
     alias: {
